afd560b6825210c9fa98de613c07db977dd32908
Jepi <extrikey@gmail.com>
2018-03-11 16:44:18 -0400

    quitbtnimplemented

--- andrio/app/src/main/java/com/example/jackhuang/slotmachine/MainActivity.java
+++ andrio/app/src/main/java/com/example/jackhuang/slotmachine/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.jackhuang.slotmachine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        //Beginning of buttons
+        Button quit = findViewById(R.id.QuitBtn);
+        quit.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+
     }
 }
