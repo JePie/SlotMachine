de21a41cf9659eb55420c98193515786b742ec18
daimenion <daimenion@hotmail.com>
2018-03-11 18:37:14 -0400

    a

--- andrio/app/src/main/java/com/example/jackhuang/slotmachine/MainActivity.java
+++ andrio/app/src/main/java/com/example/jackhuang/slotmachine/MainActivity.java
@@ -7,6 +7,8 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     //Player
@@ -14,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView pBalance;
 
     //Player Bet
-    private int bet = 0;
+    private int bet = 1;
     private TextView playerBet;
 
     //Jackpot
@@ -49,6 +51,8 @@ public class MainActivity extends AppCompatActivity {
             R.drawable.ic_luckyseven
     };
 
+    private TextView msg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,21 +60,24 @@ public class MainActivity extends AppCompatActivity {
 
         pBalance = findViewById(R.id.playerAmount);
         pBalance.setText("Player Amount: " + playerAmount);
+
         jpTxt = findViewById(R.id.currentJackpot);
         jpTxt.setText("Jackpot: " + jackpot);
+
+        msg = findViewById(R.id.msg);
 
         Slot1 = findViewById(R.id.slot1);
 
         Slot2 = findViewById(R.id.slot2);
 
-        Slot1 = findViewById(R.id.slot3);
+        Slot3 = findViewById(R.id.slot3);
 
         //Beginning of buttons
         Button quit = findViewById(R.id.QuitBtn);
         quit.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
-                finish();
+                ifcanspin();
             }
         });
 
@@ -79,8 +86,80 @@ public class MainActivity extends AppCompatActivity {
         Button reset = findViewById(R.id.Reset);
         reset.setOnClickListener(new View.OnClickListener()
                                  {
+                                     public void onClick(View v){
+                                         bet = 0;
+                                         playerBet.setText("Bet: " +bet);
+                                         msg.setText("");
+                                         spinButton.setEnabled(true);
+                                     }
+                                 });
 
-                                 }
-        );
     }
+    public void ifcanspin(){
+        if(bet == 0)
+        msg.setText("you have to place a bet");
+        if(fundsAvailable && playerAmount >= bet && bet>0){
+            runSpin();
+        }
+        else if (bet>0){
+            msg.setText("No more Money");
+
+        }
+    }
+    public void runSpin(){
+        int chance1 = new Random().nextInt(3);
+        int chance2 = new Random().nextInt(3);
+        int chance3 = new Random().nextInt(3);
+
+        Slot1.setImageResource(DRAWABLE_ID[chance1]);
+        Slot2.setImageResource(DRAWABLE_ID[chance2]);
+        Slot3.setImageResource(DRAWABLE_ID[chance3]);
+
+        if(chance1+chance2+chance3==0) {
+            msg.setText("YOU WON THE JACKPOT");
+            playerAmount +=jackpot;
+        }
+        if(chance1==chance2&&chance2==chance3){
+            msg.setText("You Win");
+            isWin = true;
+        }else{
+            isWin=false;
+        }
+        //set must win condition to increase the rate of win
+        if(play >= setWin && win < 5)
+        {
+            Slot1.setImageResource(DRAWABLE_ID[chance1]);
+            Slot2.setImageResource(DRAWABLE_ID[chance1]);
+            Slot3.setImageResource(DRAWABLE_ID[chance1]);
+            msg.setText("You Won!");
+            isWin = true;
+            play = 0;
+            setWin = new Random().nextInt(setWin)+5;
+
+            //when getting the diamond in line
+            if (chance1 == 0) {
+                playerAmount += jackpot;
+                msg.setText("You Won the Jackpot!!!");
+            }
+        }
+
+        if(isWin){
+            win++;
+            if(win >= 5) win = 0;
+            playerAmount += bet;
+            pBalance.setText(""+playerAmount);
+        }else{
+            playerAmount -= bet;
+            if(playerAmount < 0) playerAmount = 0;
+            pBalance.setText(""+playerAmount);
+            msg.setText("You Lost!");
+        }
+
+        if(playerAmount ==0){
+            bet = 0;
+            playerBet.setText("" + bet);
+        }
+
+    }
+
 }
